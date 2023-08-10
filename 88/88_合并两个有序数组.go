034291c8b4d3a0630bcebb54b82642bb5b234a74
lyeka <augustr2017@163.com>
2023-08-10 18:32:06 +0800

    test

---
+++ 88/88_合并两个有序数组.go
@@ -0,0 +1,28 @@
+package mergeSortedArray
+
+//[doc](../doc/content/88/88_合并两个有序数组.md)
+//leetcode submit region begin(Prohibit modification and deletion)
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	sorted := make([]int, 0, m+n)
+	i, j := 0, 0
+	for {
+		if i == m {
+			sorted = append(sorted, nums2[j:]...)
+			break
+		}
+		if j == n {
+			sorted = append(sorted, nums1[i:]...)
+			break
+		}
+		if nums1[i] <= nums2[j] {
+			sorted = append(sorted, nums1[i])
+			i++
+		} else {
+			sorted = append(sorted, nums2[j])
+			j++
+		}
+	}
+	copy(nums1, sorted)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ 88/88_合并两个有序数组_test.go
@@ -0,0 +1,35 @@
+package mergeSortedArray
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_merge(t *testing.T) {
+	type args struct {
+		nums1 []int
+		m     int
+		nums2 []int
+		n     int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "1",
+			args: args{
+				nums1: []int{1, 2, 3, 0, 0, 0},
+				m:     3,
+				nums2: []int{2, 5, 6},
+				n:     2,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			merge(tt.args.nums1, tt.args.m, tt.args.nums2, tt.args.n)
+			fmt.Println(tt.args.nums1)
+		})
+	}
+}
