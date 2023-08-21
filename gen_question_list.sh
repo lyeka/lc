5bcd764895aaca4daaa83a7dbf5802f59f6f8338
lyeka <augustr2017@163.com>
2023-08-21 12:11:39 +0800

    题目生成脚本计算已完成数目统计

--- gen_question_list.sh
+++ gen_question_list.sh
@@ -3,6 +3,7 @@
 # 指定目录和Markdown文件名
 target_directory="doc/content"
 markdown_file="README.md"
+count=0
 
 # 清空已存在的Markdown文件内容
 > "$markdown_file"
@@ -10,7 +11,6 @@ markdown_file="README.md"
 # 递归遍历目录及其子目录下的所有文件名，并写入Markdown文件
 echo "## 题目索引" >> "$markdown_file"
 echo "" >> "$markdown_file"
-
 # 定义递归函数
 list_files() {
     local current_directory="$1"
@@ -21,6 +21,7 @@ list_files() {
             filename=$(basename "$file" .md)  # 移除 .md 后缀
             prefix=$(echo "$filename" | grep -Eo '^[0-9]+')  # 提取数字前缀
             echo "$indent- [$filename]($file)/[Code]($prefix/$filename.go)" >> "$markdown_file"
+            count=$((count + 1))
         elif [ -d "$file" ]; then
             dirname=$(basename "$file")
             list_files "$file" "$indent  "
@@ -29,5 +30,7 @@ list_files() {
 }
 
 list_files "$target_directory" ""
+echo "" >> "$markdown_file"
+echo "Total: $count of 150" >> "$markdown_file"
 
 echo "Markdown文件已生成：$markdown_file"
