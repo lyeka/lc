61089452971d6d54bd873813feb9538c1e61ee6a
lyeka <augustr2017@163.com>
2023-11-06 17:35:40 +0800

    update 88

--- 0088/0088_合并两个有序数组.go
+++ 0088/0088_合并两个有序数组.go
@@ -2,26 +2,30 @@ package mergeSortedArray
 
 //leetcode submit region begin(Prohibit modification and deletion)
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	sorted := make([]int, 0, m+n)
-	i, j := 0, 0
-	for {
-		if i == m {
-			sorted = append(sorted, nums2[j:]...)
-			break
-		}
-		if j == n {
-			sorted = append(sorted, nums1[i:]...)
-			break
-		}
-		if nums1[i] <= nums2[j] {
-			sorted = append(sorted, nums1[i])
-			i++
+	index := m + n - 1
+	i, j := m-1, n-1
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[index] = nums1[i]
+			i--
 		} else {
-			sorted = append(sorted, nums2[j])
-			j++
+			nums1[index] = nums2[j]
+			j--
 		}
+		index--
 	}
-	copy(nums1, sorted)
+	for i >= 0 {
+		nums1[index] = nums1[i]
+		i--
+		index--
+
+	}
+
+	for j >= 0 {
+		nums1[index] = nums2[j]
+		j--
+		index--
+	}
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
