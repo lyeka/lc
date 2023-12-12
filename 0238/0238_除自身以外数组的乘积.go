7593269d1f5f9d3e1775f0985307fd9638c8fcd0
lyeka <augustr2017@163.com>
2023-12-12 11:26:23 +0800

    add 238

---
+++ 0238/0238_除自身以外数组的乘积.go
@@ -0,0 +1,22 @@
+package productOfArrayExceptSelf
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// 思路：利用前缀和计算上下三角的乘积
+func productExceptSelf(nums []int) []int {
+	ans := make([]int, len(nums))
+	ans[0] = 1
+	tmp := 1
+	for i := 1; i < len(nums); i++ {
+		ans[i] = ans[i-1] * nums[i-1]
+	}
+
+	for i := len(nums) - 2; i >= 0; i-- {
+		tmp = tmp * nums[i+1]
+		ans[i] *= tmp
+	}
+
+	return ans
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
